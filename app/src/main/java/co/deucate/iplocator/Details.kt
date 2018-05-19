98ba3b45e41f1b1394660c01fb97e3b83e3d346e
KartikPatelOfficial <patelkartik1910@gmail.com>
2018-05-20 01:48:09 +0530

    ADD: Fetch data
      Now application can fetch data and parse it for display to the user.
    
    CHANGELOG: #1

---
+++ app/src/main/java/co/deucate/iplocator/Details.kt
@@ -0,0 +1,20 @@
+package co.deucate.iplocator
+
+data class Details(
+
+        val det: String = "",
+        val city: String = "New York City",
+        val country: String = "United State of America",
+        val countryCode: String = "USA",
+        val isp: String = "Verizon telecommunication",
+        val lat: Double = 0.0,
+        val lon: Double = 0.0,
+        val org: String = "Verizon Ltd.",
+        val query: String = "192.168.0.100",
+        val religion: String = "NY",
+        val religionName: String = "New York",
+        val status: String = "Success",
+        val timeZone: String = "NY/USC",
+        val zip: String = "00000"
+
+)
--- app/src/main/java/co/deucate/iplocator/HomeActivity.kt
+++ app/src/main/java/co/deucate/iplocator/HomeActivity.kt
@@ -1,12 +1,76 @@
 package co.deucate.iplocator
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.beust.klaxon.Klaxon
+import com.shashank.sony.fancydialoglib.Animation
+import com.shashank.sony.fancydialoglib.FancyAlertDialog
+import okhttp3.*
+import java.io.IOException
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val url = "http://ip-api.com/json/203.77.200.35";
+        getData(url)
     }
+
+    private fun getData(url: String) {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+                .url(url)
+                .build()
+
+        val call = client.newCall(request)
+        call.enqueue(object : Callback {
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                showError(e)
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+
+                if (response!!.isSuccessful) {
+                    val jsonData = response.body()!!.string()
+                    parseJSON(jsonData)
+                } else {
+                    showError()
+                }
+
+
+            }
+
+        })
+
+    }
+
+    private fun parseJSON(jsonData: String) {
+        jsonData.replace("as","dat")
+        val mainDetail = Klaxon().parse<Details>(jsonData)
+        showResult(mainDetail)
+    }
+
+    private fun showResult(mainDetail: Details?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun showError(e: IOException? = null) {
+
+        if (e == null) {
+            FancyAlertDialog.Builder(this).setTitle("Error")
+                    .setBackgroundColor(Color.RED)
+                    .setMessage("Something went wrong while getting details.")
+                    .setAnimation(Animation.POP).build()
+        }else{
+            //TODO add error that change message according to IOException e
+        }
+
+    }
+
 }
+
